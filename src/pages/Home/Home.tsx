65148ee7716eb4878ff6a1f97a15cb0c95f90e01
khldoon <oi18aan@cs.umu.se>
2021-12-09 20:19:44 +0100

    Theme customization

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAddTeamEvent } from "../../shared/api";
+import { emotions } from "../../shared/types";
+import { Button } from "../../ui-lib/Button/Button";
 import { Spacer } from "../../ui-lib/Spacer/Spacer";
 
 export const Home = () => {
+	const addTeamEvent = useAddTeamEvent();
+	const [selected, setSelected] = useState(emotions[0].id);
 	return (
 		<>
 			<h1>Welcome to the golden team app!</h1>
@@ -13,6 +18,15 @@ export const Home = () => {
 			<h2>We can place the lamp-button here</h2>
 			<Spacer size={16} />
 			<p>button button button button button</p>
+			<Button
+				label="Send!"
+				onClick={async () => {
+					const result = await addTeamEvent(selected);
+					const timestamp = result?.data?.addTeamEvent.timestamp;
+
+					console.log(`event ${selected} triggered at ${timestamp}`);
+				}}
+			/>
 			<Spacer size={16} />
 
 			<Spacer size={48} />
--- src/ui-lib/Button/Button.tsx
+++ src/ui-lib/Button/Button.tsx
@@ -1,27 +1,31 @@
 import React from "react";
 import styles from "./Button.module.css";
-
+import { FcIdea } from "react-icons/fc";
 interface ButtonProps {
-  label: string;
-  loading?: boolean;
-  width?: number;
-  onClick: () => void;
+	label: string;
+	loading?: boolean;
+	width?: number;
+	onClick: () => void;
 }
 export const Button = (props: ButtonProps) => {
-  const { label, loading, width, onClick: _onClick } = props;
+	const { label, loading, width, onClick: _onClick } = props;
 
-  const text = !loading ? label : `${label}...`;
-  const style = width ? { width: `${width}px` } : undefined;
-  const onClick = !loading ? _onClick : undefined;
+	const text = !loading ? label : `${label}...`;
+	const style = width ? { width: `${width}px` } : undefined;
+	const iconStyle = { color: "black", fontSize: "2em" };
+	const onClick = !loading ? _onClick : undefined;
 
-  return (
-    <div
-      role="button"
-      className={styles.container}
-      style={style}
-      onClick={onClick}
-    >
-      <span className={styles.text}>{text}</span>
-    </div>
-  );
+	return (
+		<div
+			role="button"
+			className={styles.container}
+			style={style}
+			onClick={onClick}
+		>
+			<span className={styles.text}>{text} </span>
+			<span>
+				<FcIdea style={iconStyle} />
+			</span>
+		</div>
+	);
 };
